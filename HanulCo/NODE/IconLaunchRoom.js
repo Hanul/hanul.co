1ee411bb323ef24ba4c5a7a64dcf7277eb94fcaf
Hanul <hanul@hanul.me>
2018-04-16 13:08:29 +0900

    가로로 긴 사이즈 처리

--- HanulCo/NODE/IconLaunchRoom.js
+++ HanulCo/NODE/IconLaunchRoom.js
@@ -35,24 +35,50 @@ HanulCo.IconLaunchRoom = OBJECT({
 				
 				let filePath = rootPath + '/__RF/HanulCo/' + fileId;
 				
-				let totalCount = 0;
-				let doneCount = 0;
-				
-				EACH(HanulCo.LaunchImageMap, (size, name) => {
-					totalCount += 1;
+				IMAGEMAGICK_IDENTIFY(filePath, (features) => {
+						
+					let totalCount = 0;
+					let doneCount = 0;
 					
-					IMAGEMAGICK_CONVERT([filePath, '-resize', size.width + 'x' + size.height + '^', '-gravity', 'center', '-crop', size.width + 'x' + size.height + '+0+0', filePath + '-' + name + '.png'], {
-						error : (errorMsg) => {
-							console.log('오류가 발생했습니다. 오류 메시지: ' + errorMsg);
-						},
-						success : () => {
-							doneCount += 1;
+					if (features.width > features.height) {
+						
+						EACH(HanulCo.LaunchImageMap, (size, name) => {
+							totalCount += 1;
 							
-							if (totalCount === doneCount) {
-								ret();
-							}
-						}
-					});
+							IMAGEMAGICK_CONVERT([filePath, '-resize', size.height + 'x' + size.width + '^', '-gravity', 'center', '-crop', size.height + 'x' + size.width + '+0+0', filePath + '-' + name + '.png'], {
+								error : (errorMsg) => {
+									console.log('오류가 발생했습니다. 오류 메시지: ' + errorMsg);
+								},
+								success : () => {
+									doneCount += 1;
+									
+									if (totalCount === doneCount) {
+										ret();
+									}
+								}
+							});
+						});
+					}
+					
+					else {
+						
+						EACH(HanulCo.LaunchImageMap, (size, name) => {
+							totalCount += 1;
+							
+							IMAGEMAGICK_CONVERT([filePath, '-resize', size.width + 'x' + size.height + '^', '-gravity', 'center', '-crop', size.width + 'x' + size.height + '+0+0', filePath + '-' + name + '.png'], {
+								error : (errorMsg) => {
+									console.log('오류가 발생했습니다. 오류 메시지: ' + errorMsg);
+								},
+								success : () => {
+									doneCount += 1;
+									
+									if (totalCount === doneCount) {
+										ret();
+									}
+								}
+							});
+						});
+					}
 				});
 			});
 		});
