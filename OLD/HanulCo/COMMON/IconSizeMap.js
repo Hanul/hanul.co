21eae623b1bcfb2fc78090967011233e10c6155a
Young Jae Sim <hanul@hanul.me>
2020-08-08 16:58:27 +0900

    버그 수정

---
+++ OLD/HanulCo/COMMON/IconSizeMap.js
@@ -0,0 +1,28 @@
+HanulCo.IconSizeMap = {
+	
+	mdpi : 48,
+	hdpi : 72,
+	xhdpi : 96,
+	xxhdpi : 144,
+	xxxhdpi : 192,
+	GooglePlayStore : 512,
+	
+	iPhoneNotification2x : 40,
+	iPhoneNotification3x : 60,
+	iPhoneSettings2x : 58,
+	iPhoneSettings3x : 87,
+	iPhoneSpotlight2x : 80,
+	iPhoneSpotlight3x : 120,
+	iPhoneApp2x : 120,
+	iPhoneApp3x : 180,
+	iPadNotification1x : 20,
+	iPadNotification2x : 40,
+	iPadSettings1x : 29,
+	iPadSettings2x : 58,
+	iPadSpotlight1x : 40,
+	iPadSpotlight2x : 80,
+	iPadApp1x : 76,
+	iPadApp2x : 152,
+	iPadProApp2x : 167,
+	AppStore : 1024
+};
--- OLD/HanulCo/NODE/IconLaunchRoom.js
+++ OLD/HanulCo/NODE/IconLaunchRoom.js
@@ -16,7 +16,7 @@ HanulCo.IconLaunchRoom = OBJECT({
 				EACH(HanulCo.IconSizeMap, (size, name) => {
 					totalCount += 1;
 					
-					IMAGEMAGICK_CONVERT([filePath, '-resize', size + 'x' + size + '^', '-gravity', 'center', '-crop', size + 'x' + size + '+0+0', filePath + '-' + name + '.png'], {
+					GRAPHICSMAGICK_CONVERT([filePath, '-resize', size + 'x' + size + '^', '-gravity', 'center', '-crop', size + 'x' + size + '+0+0', filePath + '-' + name + '.png'], {
 						error : (errorMsg) => {
 							console.log('오류가 발생했습니다. 오류 메시지: ' + errorMsg);
 						},
@@ -35,7 +35,7 @@ HanulCo.IconLaunchRoom = OBJECT({
 				
 				let filePath = rootPath + '/__RF/HanulCo/' + fileId;
 				
-				IMAGEMAGICK_IDENTIFY(filePath, (features) => {
+				GRAPHICSMAGICK_IDENTIFY(filePath, (features) => {
 						
 					let totalCount = 0;
 					let doneCount = 0;
@@ -45,7 +45,7 @@ HanulCo.IconLaunchRoom = OBJECT({
 						EACH(HanulCo.LaunchImageMap, (size, name) => {
 							totalCount += 1;
 							
-							IMAGEMAGICK_CONVERT([filePath, '-resize', size.height + 'x' + size.width + '^', '-gravity', 'center', '-crop', size.height + 'x' + size.width + '+0+0', filePath + '-' + name + '.png'], {
+							GRAPHICSMAGICK_CONVERT([filePath, '-resize', size.height + 'x' + size.width + '^', '-gravity', 'center', '-crop', size.height + 'x' + size.width + '+0+0', filePath + '-' + name + '.png'], {
 								error : (errorMsg) => {
 									console.log('오류가 발생했습니다. 오류 메시지: ' + errorMsg);
 								},
@@ -65,7 +65,7 @@ HanulCo.IconLaunchRoom = OBJECT({
 						EACH(HanulCo.LaunchImageMap, (size, name) => {
 							totalCount += 1;
 							
-							IMAGEMAGICK_CONVERT([filePath, '-resize', size.width + 'x' + size.height + '^', '-gravity', 'center', '-crop', size.width + 'x' + size.height + '+0+0', filePath + '-' + name + '.png'], {
+							GRAPHICSMAGICK_CONVERT([filePath, '-resize', size.width + 'x' + size.height + '^', '-gravity', 'center', '-crop', size.width + 'x' + size.height + '+0+0', filePath + '-' + name + '.png'], {
 								error : (errorMsg) => {
 									console.log('오류가 발생했습니다. 오류 메시지: ' + errorMsg);
 								},
